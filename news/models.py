0b67ba70e495f310d522a432e087e0073db75933
AnderDev04 <desarrollador.fe@trigono.pe>
2024-08-21 14:31:28 -0500

    category

--- news/models.py
+++ news/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Creare el modelo News con estilo PEP8
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
 class News(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
@@ -8,6 +16,7 @@ class News(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
